f76bbae3c870814dff53a1c043947a5873fbdb8f
Gorans-989 <gorans989@yahoo.com>
2021-10-07 14:06:50 +0200

    0

--- Using_MongoDb_Mongoose/controllers/error.js
+++ Using_MongoDb_Mongoose/controllers/error.js
@@ -1,5 +1,5 @@
 export function get404(req, res, next) {
-    res.status(404).render('PageNotFound', {
+    res.status(404).render('404.ejs', {
         pageTitle: "Page not Found",
         path: ''
     });
