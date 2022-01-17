ff66b8367c84c5c5c3c60e4368f829f053ec27c0
Gorans-989 <gorans989@yahoo.com>
2022-01-17 12:06:56 +0100

    adding user router to base router

--- NodeJS_app_sequelize/routes/baseRouter.js
+++ NodeJS_app_sequelize/routes/baseRouter.js
@@ -1,7 +1,8 @@
 import express from "express";
 import {router as movieRouter} from "./movieRoutes";
+import {router as userRouter} from "./userRoutes";
+
 const router = express.Router();
 
-
+router.use(userRouter);
 router.use(movieRouter);
-
